abf59d7c006f593d842633b1c146326f416e07a1
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-04-25 17:38:55 +0200

    implentation of Iem class 2.0

--- WP2/P2/src/ch/hearc/wp2/p2/jeu/items/Item.java
+++ WP2/P2/src/ch/hearc/wp2/p2/jeu/items/Item.java
@@ -1,53 +1,53 @@
 
 package ch.hearc.wp2.p2.jeu.items;
 
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 public abstract class Item
 	{
 
-	//position
-	private double xPos;
-	private double yPos;
+	//origin-position0
+	private Point2D.Double pt0;
+	//size-position1
+	private Point2D.Double pt1;
 
 	//visible
 	private boolean isVisible;
 
-	//size
-	private double width;
-	private double height;
-
 	//rectangle
 	private Rectangle2D.Double rect;
 
+	//constructeurs
 	public Item(double x, double y, double w, double h, boolean v)
 		{
-		this.xPos = x;
-		this.yPos = y;
-		this.height = h;
-		this.width = w;
+		this.pt0 = new Point2D.Double(x, y);
+		this.pt1 = new Point2D.Double(x + w, x + h);
 		this.isVisible = v;
-		this.rect = new Rectangle2D.Double(x, y, w, h);
+		this.rect = new Rectangle2D.Double(pt0.x, pt0.y, pt1.x, pt1.y);
 		}
 
-	public double getxPos()
+	public Item(Item it)
 		{
-		return this.xPos;
+		this(it.getPt0().x, it.getPt0().y, it.getPt1().x, it.getPt1().y, it.isVisible);
 		}
 
-	public void setxPos(int xPos)
+	public Item(Rectangle2D.Double srect, boolean v)
 		{
-		this.xPos = xPos;
+		this.pt0 = new Point2D.Double(srect.x, srect.y);
+		this.pt1 = new Point2D.Double(srect.x + pt0.x, srect.y + pt0.y);
+		this.isVisible = v;
 		}
 
-	public double getyPos()
+	//getters & setters
+	public Point2D.Double getPt0()
 		{
-		return this.yPos;
+		return pt0;
 		}
 
-	public void setyPos(int yPos)
+	public Point2D.Double getPt1()
 		{
-		this.yPos = yPos;
+		return pt1;
 		}
 
 	public boolean isVisible()
@@ -55,24 +55,32 @@ public abstract class Item
 		return this.isVisible;
 		}
 
+	public double getHeight()
+		{
+		return pt1.y - pt0.y;
+		}
+
+	public double getWidth()
+		{
+		return pt1.x - pt0.x;
+		}
+
+	public Rectangle2D.Double getRect()
+		{
+		return rect;
+		}
+
 	public void setVisible(boolean isVisible)
 		{
 		this.isVisible = isVisible;
 		}
 
-	public double getWidth()
-		{
-		return this.width;
-		}
 
-	public double getHeight()
+	//moving the rectanle to the point pt
+	public void moveTo(Point2D.Double pt)
 		{
-		return this.height;
-		}
-
-	public Rectangle2D.Double getRect()
-		{
-		return new Rectangle2D.Double(this.xPos, this.yPos, this.width, this.height);
+		pt0 = pt;
+		rect.setRect(pt.x, pt.y, pt.x + this.getWidth(), pt.y + this.getHeight());
 		}
 
 	@Override
@@ -80,5 +88,4 @@ public abstract class Item
 		{
 		return "Item [rect=" + this.rect + "isVisible=" + this.isVisible + "]";
 		}
-
 	}
