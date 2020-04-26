7fb1d51600544a26655caf95832fba9db2e37d16
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-04-26 14:31:31 +0200

    creating about class

---
+++ WP2/P2/src/ch/hearc/wp2/p2/jeu/menus/About.java
@@ -0,0 +1,13 @@
+package ch.hearc.wp2.p2.jeu.menus;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+
+@SuppressWarnings("serial")
+public class About extends Box {
+
+	public About() {
+		super(BoxLayout.Y_AXIS);
+		
+	}
+}
