427b1a78dbc99352fd8c97d41998f5d7dc66c395
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-05-04 22:39:36 +0200

    ActionBloc interface + all class prototyped

--- WP2/P2/src/ch/hearc/wp2/p2/jeu/items/blocs/actions/ArrivalBloc.java
+++ WP2/P2/src/ch/hearc/wp2/p2/jeu/items/blocs/actions/ArrivalBloc.java
@@ -1,29 +1,33 @@
 
 package ch.hearc.wp2.p2.jeu.items.blocs.actions;
 
+import java.awt.Image;
+import java.awt.geom.Rectangle2D.Double;
 
+import ch.hearc.wp2.p2.jeu.Map;
+import ch.hearc.wp2.p2.jeu.items.blocs.Bloc;
 
-public class ArrivalBloc
-	{
+public class ArrivalBloc extends Bloc implements ActionBloc {
 
-	/*------------------------------------------------------------------*\
-	|*							Constructeurs							*|
-	\*------------------------------------------------------------------*/
-
-	/*------------------------------------------------------------------*\
-	|*							Methodes Public							*|
-	\*------------------------------------------------------------------*/
-
-	/*------------------------------*\
-	|*				Get				*|
-	\*------------------------------*/
-
-	/*------------------------------------------------------------------*\
-	|*							Methodes Private						*|
-	\*------------------------------------------------------------------*/
-
-	/*------------------------------------------------------------------*\
-	|*							Attributs Private						*|
-	\*------------------------------------------------------------------*/
+	public ArrivalBloc(Bloc it) {
+		super(it);
+		// TODO Auto-generated constructor stub
 	}
 
+	public ArrivalBloc(Double rect, boolean v, Image texture) {
+		super(rect, v, texture);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArrivalBloc(double x, double y, double w, double h, boolean v, Image texture) {
+		super(x, y, w, h, v, texture);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void interract(Map map) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
