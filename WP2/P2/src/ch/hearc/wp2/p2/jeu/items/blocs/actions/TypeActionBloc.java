04b2c2a5268d138321065680f6ff2c825f399ae5
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-05-04 22:27:28 +0200

    removing useless enum

--- WP2/P2/src/ch/hearc/wp2/p2/jeu/items/blocs/actions/TypeActionBloc.java
+++
@@ -1,5 +0,0 @@
-package ch.hearc.wp2.p2.jeu.items.blocs.actions;
-
-public enum TypeActionBloc {
-	ARRIVAL, CHECKPOINT, START;
-}
--- WP2/P2/src/ch/hearc/wp2/p2/jeu/items/blocs/traps/TypeTrapBloc.java
+++
@@ -1,5 +0,0 @@
-package ch.hearc.wp2.p2.jeu.items.blocs.traps;
-
-public enum TypeTrapBloc {
-	SPIKE, FALL;
-}
