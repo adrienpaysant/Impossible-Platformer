c64b6fc813a550067e7ce2871ede175e2f18e875
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-05-04 13:26:11 +0200

    updating shopImage with all textures

--- WP2/P2/src/ch/hearc/wp2/p2/jeu/tools/image/ShopImage.java
+++ WP2/P2/src/ch/hearc/wp2/p2/jeu/tools/image/ShopImage.java
@@ -3,10 +3,39 @@ package ch.hearc.wp2.p2.jeu.tools.image;
 
 import javax.swing.ImageIcon;
 
-public class ShopImage
-	{
-	
+public class ShopImage {
+
+	// blocks
 	public static final ImageIcon PATHBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/stonePathBlock.png");
 	public static final ImageIcon DIRTBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/dirtBlock.png");
+	public static final ImageIcon FORESTBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/forestBlock.png");
+	public static final ImageIcon CAVEBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/caveBlock.png");
+	public static final ImageIcon DARKDIRTBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/darkDirtBlock.png");
+	public static final ImageIcon DARKGRASSDIRTBLOCK = ImageLoader
+			.loadAsynchroneJar("ressources/Blocks/darkGrassDirtBlock.png");
+	public static final ImageIcon DARKSKYBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/darkSkyBlock.png");
+	public static final ImageIcon DARKSTONEBLOCK = ImageLoader
+			.loadAsynchroneJar("ressources/Blocks/darkStoneBlock.png");
+	public static final ImageIcon GRASSDIRTBLOCK = ImageLoader
+			.loadAsynchroneJar("ressources/Blocks/grassDirtBlock.png");
+	public static final ImageIcon ICEBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/iceBlock.png");
+	public static final ImageIcon ICEBLOCKTOP = ImageLoader.loadAsynchroneJar("ressources/Blocks/iceBlockTop.png");
+	public static final ImageIcon ICEDIRTBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/iceDirtBlock.png");
+	public static final ImageIcon ICESTONEPATHBLOCK = ImageLoader
+			.loadAsynchroneJar("ressources/Blocks/iceStonePathBlock.png");
+	public static final ImageIcon LEAVESBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/leavesBlock.png");
+	public static final ImageIcon PURPLEBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/purpleBlock.png");
+	public static final ImageIcon SANDBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/sandBlock.png");
+	public static final ImageIcon SKYBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/skyBlock.png");
+	public static final ImageIcon STONEBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/stoneBlock.png");
+	public static final ImageIcon STONEPATHBLOCK = ImageLoader
+			.loadAsynchroneJar("ressources/Blocks/stonePathBlock.png");
+	public static final ImageIcon WATERBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/waterBlock.png");
+	public static final ImageIcon WATERTOPBLOCK = ImageLoader.loadAsynchroneJar("ressources/Blocks/waterTopBlock.png");
 
-	}
+	// elements
+	public static final ImageIcon COIN = ImageLoader.loadAsynchroneJar("ressources/Blocks/coin.png");
+	public static final ImageIcon HEART = ImageLoader.loadAsynchroneJar("ressources/Blocks/heart.png");
+	public static final ImageIcon SPIKES = ImageLoader.loadAsynchroneJar("ressources/Blocks/spikes.png");
+
+}
