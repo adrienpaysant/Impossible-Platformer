5f1e2091bd7e9300a86dcc803c3e9cb92742f746
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-05-04 22:37:25 +0200

    trapBloc interface & spike & fall prototyped

--- WP2/P2/src/ch/hearc/wp2/p2/jeu/items/blocs/actions/ActionBloc.java
+++ WP2/P2/src/ch/hearc/wp2/p2/jeu/items/blocs/actions/ActionBloc.java
@@ -1,6 +1,8 @@
 
 package ch.hearc.wp2.p2.jeu.items.blocs.actions;
 
+import ch.hearc.wp2.p2.jeu.Map;
+
 public interface ActionBloc {
-	public void interract();
+	public void interract(Map map);
 }
--- WP2/P2/src/ch/hearc/wp2/p2/jeu/items/blocs/traps/FallBloc.java
+++ WP2/P2/src/ch/hearc/wp2/p2/jeu/items/blocs/traps/FallBloc.java
@@ -1,29 +1,35 @@
 
 package ch.hearc.wp2.p2.jeu.items.blocs.traps;
 
+import java.awt.Image;
+import java.awt.geom.Rectangle2D.Double;
+
+import ch.hearc.wp2.p2.jeu.Map;
+import ch.hearc.wp2.p2.jeu.items.blocs.Bloc;
+
+public class FallBloc extends Bloc implements TrapBloc {
 
 
-public class FallBloc
-	{
-
-	/*------------------------------------------------------------------*\
-	|*							Constructeurs							*|
-	\*------------------------------------------------------------------*/
-
-	/*------------------------------------------------------------------*\
-	|*							Methodes Public							*|
-	\*------------------------------------------------------------------*/
-
-	/*------------------------------*\
-	|*				Get				*|
-	\*------------------------------*/
-
-	/*------------------------------------------------------------------*\
-	|*							Methodes Private						*|
-	\*------------------------------------------------------------------*/
-
-	/*------------------------------------------------------------------*\
-	|*							Attributs Private						*|
-	\*------------------------------------------------------------------*/
+	public FallBloc(Bloc it) {
+		super(it);
+		// TODO Auto-generated constructor stub
 	}
 
+	public FallBloc(Double rect, boolean v, Image texture) {
+		super(rect, v, texture);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FallBloc(double x, double y, double w, double h, boolean v, Image texture) {
+		super(x, y, w, h, v, texture);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void trapAction(Map map) {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
