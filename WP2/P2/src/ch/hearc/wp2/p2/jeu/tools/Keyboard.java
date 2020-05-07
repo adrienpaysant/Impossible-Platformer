5172924cf0c1b55c39a0e4c15192226c89ef108b
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-05-07 15:28:00 +0200

    updating KeyBoard to have good movements horizontaly

--- WP2/P2/src/ch/hearc/wp2/p2/jeu/tools/Keyboard.java
+++ WP2/P2/src/ch/hearc/wp2/p2/jeu/tools/Keyboard.java
@@ -8,6 +8,19 @@ import ch.hearc.wp2.p2.jeu.Map;
 
 public class Keyboard implements KeyListener {
 
+	private boolean left = false;
+	private boolean right = false;
+	private String whoIsLast;
+
+	private void test() {
+		if (left && whoIsLast=="left")
+			Map.getInstance().setdX(-1);
+		else if (right && whoIsLast=="right")
+			Map.getInstance().setdX(1);
+		else
+			Map.getInstance().setdX(0);
+	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 	}
@@ -16,19 +29,30 @@ public class Keyboard implements KeyListener {
 	public void keyPressed(KeyEvent e) {
 		// left
 		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-			Map.getInstance().setdX(-1);
+			this.left = true;
+			this.whoIsLast="left";
 		} // right
 		else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			Map.getInstance().setdX(1);
+			this.right = true;
+			this.whoIsLast="right";
 		}
-
+		test();
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-
-		Map.getInstance().setdX(0);
-
+		// left
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			this.left = false;
+			if(whoIsLast=="left" && right)
+				whoIsLast="right";
+		} // right
+		else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			this.right = false;
+			if(whoIsLast=="right" && left)
+				whoIsLast="left";
+		}
+		test();
 	}
 
 }
