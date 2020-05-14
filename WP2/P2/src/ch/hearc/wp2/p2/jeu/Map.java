2f082e9761727ec9e22930765f84d154a1db31f4
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-05-14 12:28:09 +0200

    first implementation of leaderboard page

--- WP2/P2/src/ch/hearc/wp2/p2/jeu/Map.java
+++ WP2/P2/src/ch/hearc/wp2/p2/jeu/Map.java
@@ -39,8 +39,8 @@ public class Map extends JPanel {
 	private static final int PLAYER_W = 25;
 	public static final int GRAVITY = 4;
 	private static final int HEART_WH = 25;
-	private static final int PLAYER_NB_LIFE = 10;
-	private static final boolean DEBUG = true;
+	private static final int PLAYER_NB_LIFE = 3;
+	private static final boolean DEBUG = false;
 
 	private Game game;
 	private ExitButton exitButton;
--- WP2/P2/src/ch/hearc/wp2/p2/jeu/menus/LeaderBoard.java
+++ WP2/P2/src/ch/hearc/wp2/p2/jeu/menus/LeaderBoard.java
@@ -1,21 +1,27 @@
 package ch.hearc.wp2.p2.jeu.menus;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import ch.hearc.wp2.p2.jeu.Game;
-
+import ch.hearc.wp2.p2.jeu.tools.ExitButton;
+import ch.hearc.wp2.p2.jeu.tools.position.JCenterH;
 
 @SuppressWarnings("serial")
 public class LeaderBoard extends JPanel {
 
 	private static LeaderBoard leaderBoard = null;
-
-	private Game game;
-	private JButton buttonExit;
+	private Image bgImage;
 	
+	private Game game;
+	private ExitButton exitButton;
+
 	// singleton
 	public static LeaderBoard getInstance() {
 		if (leaderBoard == null)
@@ -26,12 +32,29 @@ public class LeaderBoard extends JPanel {
 	private LeaderBoard() {
 
 		this.game = Game.getInstance();
-		this.buttonExit = new JButton("Back to Menu");
+		this.exitButton = new ExitButton("Back to Menu", "MainMenu");
+		
+		try {
+			bgImage = ImageIO.read(getClass().getResource("/images/menubg2.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		add(new JCenterH(exitButton));
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-
+		Graphics2D g2D = (Graphics2D) g;
+		draw(g2D);
 	}
+
+	private void draw(Graphics2D g2d) {
+		
+		g2d.drawImage(bgImage, 0, 0, getWidth(), getHeight(), 0, 0, bgImage.getWidth(null), bgImage.getHeight(null),
+				null);
+	
+	}
+
 }
