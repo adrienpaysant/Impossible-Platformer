e60e6b29d8d36fd0b58a33979fbb5e0973fc9730
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-05-16 13:29:10 +0200

    creating Audio class + adding mario type sounds

---
+++ WP2/P2/src/ch/hearc/wp2/p2/jeu/tools/Audio.java
@@ -0,0 +1,44 @@
+package ch.hearc.wp2.p2.jeu.tools;
+
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Audio {
+	private Clip clip;
+
+	public Audio(String path) throws UnsupportedAudioFileException, IOException {
+
+		try {
+			AudioInputStream audio = AudioSystem.getAudioInputStream(getClass().getResource(path));
+			clip = AudioSystem.getClip();
+			clip.open(audio);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	// getter
+	public Clip getClip() {
+		return clip;
+	}
+
+	// methodes
+	public void play() {
+		clip.start();
+	}
+
+	public void stop() {
+		clip.stop();
+	}
+
+	public static void playSound(String path) throws UnsupportedAudioFileException, IOException {
+		Audio s = new Audio(path);
+		s.play();
+	}
+
+}
