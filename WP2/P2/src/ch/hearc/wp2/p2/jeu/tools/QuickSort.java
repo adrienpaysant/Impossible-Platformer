432097074753554bf29dd5c4357f531e9c85ff9e
Monnet Joris <joris.monnet@he-arc.ch>
2020-05-30 02:06:37 +0200

    add quicksort for leaderboard

---
+++ WP2/P2/src/ch/hearc/wp2/p2/jeu/tools/QuickSort.java
@@ -0,0 +1,35 @@
+package ch.hearc.wp2.p2.jeu.tools;
+
+class QuickSort {
+	int partition(int arr[], int low, int high) {
+		int pivot = arr[high];
+		int i = (low - 1); // index of smaller element
+		for (int j = low; j < high; j++) {
+			if (arr[j] < pivot) {
+				int temp = arr[++i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		int temp = arr[++i];
+		arr[i] = arr[high];
+		arr[high] = temp;
+
+		return i;
+	}
+
+	void sort(int arr[], int low, int high) {
+		if (low < high) {
+			int pivot = partition(arr, low, high);
+			sort(arr, low, pivot - 1);
+			sort(arr, pivot + 1, high);
+		}
+	}
+
+	public static int[] useSort(int arr[]) {
+		QuickSort ob = new QuickSort();
+		ob.sort(arr, 0, arr.length - 1);
+		return arr;
+	}
+	
+}
